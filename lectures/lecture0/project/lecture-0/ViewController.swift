e61c7ba6164fdd33bc65fd8124ada248c783b137
Maksym Savisko <stowyn@gmail.com>
2018-10-27 18:48:44 +0300

    feat: add simple values playground feat: add snippets for playground docs: create readme for subfolders

---
+++ lectures/lecture0/project/lecture-0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lecture-0
+//
+//  Created by Maksym Savisko on 10/27/18.
+//  Copyright © 2018 Maksym Savisko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
