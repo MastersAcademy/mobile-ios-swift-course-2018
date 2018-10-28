1465f0d5564ab08f1496ac635b3d550d42af95ea
Maksym Savisko <stowyn@gmail.com>
2018-10-28 22:48:43 +0200

    feat: add playground for function and closure parts

---
+++ lectures/lecture0/project/003-Functions-and-Closures.playground/Contents.swift
@@ -0,0 +1,11 @@
+import PlaygroundSupport
+import Foundation
+import UIKit
+
+//001. Simple function
+func greet(person: String, day: String) -> String {
+    return "Hello \(person), today is \(day)."
+}
+greet(person: "Bob", day: "Tuesday")
+
+
