b33cf881e76c2f79b726ace1d267ce69307bd3c2
Maksym Savisko <stowyn@gmail.com>
2018-10-29 20:49:31 +0200

    feat: add function closures playground

--- lectures/lecture0/project/003-Functions-and-Closures.playground/Contents.swift
+++ lectures/lecture0/project/003-Functions-and-Closures.playground/Contents.swift
@@ -8,4 +8,79 @@ func greet(person: String, day: String) -> String {
 }
 greet(person: "Bob", day: "Tuesday")
 
+//002. Function with no argument label
+func greet(_ person: String, on day: String) -> String {
+    return "Hello \(person), today is \(day)."
+}
+greet("John", on: "Wednesday")
 
+//003. Function with tuple as return value
+func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+    var min = scores[0]
+    var max = scores[0]
+    var sum = 0
+    
+    for score in scores {
+        if score > max {
+            max = score
+        } else if score < min {
+            min = score
+        }
+        sum += score
+    }
+    
+    return (min, max, sum)
+}
+let statistics = calculateStatistics(scores: [5, 3, 100, 3, 9])
+print(statistics.sum)
+print(statistics.2)
+
+//004. Nested functions
+func returnFifteen() -> Int {
+    var y = 10
+    func add() {
+        y += 5
+    }
+    add()
+    return y
+}
+returnFifteen()
+
+//005. Function as return value
+func makeIncrementer() -> ((Int) -> Int) {
+    func addOne(number: Int) -> Int {
+        return 1 + number
+    }
+    return addOne
+}
+var increment = makeIncrementer()
+increment(7)
+
+//006. Function as argument
+func hasAnyMatches(list: [Int], condition: (Int) -> Bool) -> Bool {
+    for item in list {
+        if condition(item) {
+            return true
+        }
+    }
+    return false
+}
+func lessThanTen(number: Int) -> Bool {
+    return number < 10
+}
+var numbers = [20, 19, 7, 12]
+hasAnyMatches(list: numbers, condition: lessThanTen)
+
+//007. Simple closures
+numbers.map({ (number: Int) -> Int in
+    let result = 3 * number
+    return result
+})
+
+//008. Ommit type of function parameter
+let mappedNumbers = numbers.map({ number in 3 * number })
+print(mappedNumbers)
+
+//009. Parameter to parameter by number
+let sortedNumbers = numbers.sorted { $0 > $1 }
+print(sortedNumbers)
