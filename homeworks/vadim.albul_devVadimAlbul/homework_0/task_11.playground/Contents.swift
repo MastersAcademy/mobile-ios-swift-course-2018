108ca3f2468fc060f546c815ec415f1bff746efc
Vadim Albul <vadim.albul.290992@gmail.com>
2018-11-01 17:33:05 +0200

    - working with task 11

---
+++ homeworks/vadim.albul_devVadimAlbul/homework_0/task_11.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+
+// MARK: 011 - crash: Execution was interrupted
+let text: String? = nil
+var item2 = text!
+
+let arr = ["1", "2"]
+let a = arr[2]
+
+// MARK: 011 - short code for crash
+var item: Int = Int("r")!
+
+
